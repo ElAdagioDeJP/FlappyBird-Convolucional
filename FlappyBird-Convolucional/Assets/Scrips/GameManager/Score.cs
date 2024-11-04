5827ddb47467816b5cc6a8f320a47e9c07fb9901
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-11-04 01:17:37 -0400

    Alpha 1.1.7 Se creo la funcionalidad para el cambio de bioma en el juego Falta: Hacer las nuevas scenas (1vs1, 2vs2) Hacer el modo Arcade Con dicho modo sus enemigos

--- FlappyBird-Convolucional/Assets/Scrips/GameManager/Score.cs
+++ FlappyBird-Convolucional/Assets/Scrips/GameManager/Score.cs
@@ -7,8 +7,18 @@ public class Score : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI bestScore;
+    public GameObject Montain;
+    public GameObject MontainSpawn;
+    public GameObject Desert;
+    public GameObject DesertSpawn;
+    public GameObject Snow;
+    public GameObject SnowSpawn;
+    public GameObject Dark;
+    public GameObject DarkSpawn;
     private int score;
-
+    int j = 0;
+    int i = 0;
+    float ChangeBiome = 3.5f;
     void Start()
     {
         scoreText.text = score.ToString();
@@ -25,8 +35,58 @@ public class Score : MonoBehaviour
     }
     public void UpdateScore()
     {
+        j = Random.Range(1, 100);
+        
+        if( j > 0 && j < 5)
+        {
+            i++;
+            StartCoroutine(CambioBioma());
+            if(i == 4)
+            {
+                i = 0;
+            }
+            
+        }
+
         score++;
         scoreText.text = score.ToString();
         UpdateBestScore();
+    }
+    IEnumerator CambioBioma()
+    {
+        if (i == 0 || i > 3)
+        {
+            Dark.SetActive(false);
+            DarkSpawn.SetActive(false);
+            Montain.SetActive(true);
+            yield return new WaitForSeconds(ChangeBiome);
+            MontainSpawn.SetActive(true);
+        }
+        else if (i == 1)
+        {
+            Montain.SetActive(false);
+            MontainSpawn.SetActive(false);
+            Desert.SetActive(true);
+            yield return new WaitForSeconds(ChangeBiome);
+            DesertSpawn.SetActive(true);
+        }
+        else if (i == 2)
+        {
+            Desert.SetActive(false);
+            DesertSpawn.SetActive(false);
+            Snow.SetActive(true);
+            yield return new WaitForSeconds(ChangeBiome);
+            SnowSpawn.SetActive(true);
+        }
+        else if (i == 3)
+        {
+            Snow.SetActive(false);
+            SnowSpawn.SetActive(false);
+            Dark.SetActive(true);
+            yield return new WaitForSeconds(ChangeBiome);
+            DarkSpawn.SetActive(true);
+        }
+        
+
     }
 }
